48ce9583c341ed9239ed073a0e613fcee1903918
April Ashton <aprilashton18@gmail.com>
2022-12-13 06:22:08 -0800

    commit tree e4208b99f22e111103e83d3bdd7a8e66a6967294

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let headquarters = 42;
+    return Math.abs(headquarters - location);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start)) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let under2000; 
+    let over2000;
+    if (distance > 0 && distance < 400) {
+        return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        under2000 = (distance - 400) * 0.02;
+        return under2000;
+    } else if (distance > 2000 && distance < 2500){
+        over2000 = 25;
+        return over2000;
+    } else {
+        return "cannot travel that far";
+    }
+
+}
